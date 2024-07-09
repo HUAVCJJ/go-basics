dc543dd86100f3d01594dd2ca9a436500e7328f5
HUAVCJJ <huavcjj0217@icloud.com>
2024-07-09 14:43:57 +0900

    init

---
+++ 01-variables/main.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+//const secret = "abc"
+
+type Os int
+
+const (
+	Mac Os = iota + 1
+	Windows
+	Linux
+)
+
+/*
+var (
+	i int
+	s string
+	b bool
+)
+*/
+
+func main() {
+	//var i int
+	//var i int = 2
+	//var i = 2
+	i := 1
+	ui := uint16(1)
+	fmt.Println(i)
+	fmt.Printf("i: %v %T\n", i, i)
+	fmt.Printf("i: %[1]v %[1]T ui: %[2]v %[2]T\n", i, ui)
+
+	f := 1.23456
+	s := "hello"
+	b := true
+	fmt.Printf("f: %[1]v %[1]T\n", f)
+	fmt.Printf("s: %[1]v %[1]T\n", s)
+	fmt.Printf("b: %[1]v %[1]T\n", b)
+
+	pi, title := 3.14, "Go"
+	fmt.Printf("pi: %v title: %v\n", pi, title)
+
+	x := 10
+	y := 1.23
+	z := float64(x) + y
+	fmt.Println(z)
+
+	fmt.Printf("Mac: %v Windows: %v Linux: %v\n", Mac, Windows, Linux)
+
+	i = 2
+	fmt.Printf("i: %v\n", i)
+	i += 1
+	fmt.Printf("i: %v\n", i)
+	i *= 2
+	fmt.Printf("i: %v\n", i)
+}
